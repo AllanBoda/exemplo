f49e66d88702ddc922382e42e2cb78356876bb24
AllanBoda <ldgdanielgomes@gmail.com>
2023-06-13 18:02:33 -0300

    Atualizacao 2

--- src/br/org/catolicasc/cliente/GreetClient.java
+++ src/br/org/catolicasc/cliente/GreetClient.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class GreetClient {
     private Socket clientSocket;
@@ -13,10 +14,25 @@ public class GreetClient {
 
     public void start(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
-        // handler para escrita de dados
+        // Handler para escrita de dados
         out = new PrintWriter(clientSocket.getOutputStream(), true);
-        // handler lara leitura de dados
+        // Handler para leitura de dados
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        clientHandler();
+    }
+
+    private void clientHandler() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String message;
+        String response;
+        do {
+            System.out.print("Client: ");
+            message = scanner.nextLine();
+            out.println(message); // Manda a mensagem para o socket
+            response = in.readLine(); // Recebe a resposta do socket
+            System.out.println("Server: " + response);
+        } while (!message.equals("!quit"));
     }
 
     public void stop() {
@@ -29,17 +45,10 @@ public class GreetClient {
         }
     }
 
-    public String sendMessage(String msg) throws IOException {
-        out.println(msg);  // manda a msg para o socket
-        return in.readLine();  // retorna a mensagem recebida do socket
-    }
-
     public static void main(String[] args) {
         GreetClient client = new GreetClient();
         try {
             client.start("127.0.0.1", 12345);
-            String response = client.sendMessage("hello server");
-            System.out.println("Resposta do servidor: " + response);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         } finally {
--- src/br/org/catolicasc/server/GreetServer.java
+++ src/br/org/catolicasc/server/GreetServer.java
@@ -1,35 +1,49 @@
 package br.org.catolicasc.server;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class GreetServer {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+
     public void start(int port) throws IOException {
-        // inicializar atributos
-        serverSocket = new ServerSocket(port);  // escuta na porta port
-        clientSocket = serverSocket.accept();  // espera conexão
-        // handler para escrita de dados
-        out = new PrintWriter(clientSocket.getOutputStream());
-        // handler lara leitura de dados
+        // Inicializar atributos
+        serverSocket = new ServerSocket(port); // Escuta na porta port
+        clientSocket = serverSocket.accept(); // Espera conexão
+
+        // Handler para escrita de dados
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        // Handler para leitura de dados
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         clientHandler();
     }
 
     private void clientHandler() throws IOException {
-        String greeting = in.readLine();
-        if ("hello server".equals(greeting)) {
-            out.println("hello client");
-        } else {
-            out.println("Mensagem incorreta.");
-        }
+        String message;
+        do {
+            message = in.readLine();
+            if (message != null) {
+                System.out.println("Client: " + message);
+                if ("!quit".equals(message)) {
+                    out.println("Server disconnected.");
+                    break;
+                } else {
+                    System.out.print("Server: ");
+                    String response = System.console().readLine();
+                    out.println(response);
+                }
+            }
+        } while (true);
     }
+
     public void stop() {
         try {
             in.close();
@@ -40,6 +54,7 @@ public class GreetServer {
             System.out.println("Erro ao fechar a conexão.");
         }
     }
+
     public static void main(String[] args) {
         GreetServer server = new GreetServer();
         try {
